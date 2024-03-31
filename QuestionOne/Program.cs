31d023c38057ed48870a38194a7629c6e8b38606
Omar Dachia <omardachia@gmail.com>
2024-03-31 11:45:33 +0100

    this is a Program the dispaly any Message on the screen

---
+++ QuestionOne/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1: Write a program to print any massage a on the screen
+            Console.WriteLine("Q1:-> Write a program to print any massage a on the Screen");
+            Console.WriteLine("===========================================================");
+            Console.WriteLine("Solution:->");
+            Console.WriteLine("+++++++++++");
+
+            Console.WriteLine("Any Massage");
+            Console.ReadKey();
+
+
+        }
+    }
+}
